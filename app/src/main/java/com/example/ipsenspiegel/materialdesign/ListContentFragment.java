0d667d6e7ed3304ba021596a2ae8bf24000e730e
John <serradillamate.alejandro@gmail.com>
2016-11-30 16:27:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ipsenspiegel/materialdesign/ListContentFragment.java
@@ -0,0 +1,15 @@
+package com.example.ipsenspiegel.materialdesign;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListContentFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list_content, null);
+    }
+}
---
+++ app/src/main/java/com/example/ipsenspiegel/materialdesign/TileContentFragment.java
@@ -0,0 +1,15 @@
+package com.example.ipsenspiegel.materialdesign;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TileContentFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tile_content, null);
+    }
+}
